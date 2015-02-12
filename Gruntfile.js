e570205f9adbbe4444819100d6330128fb5244e2
cyurtbil <cenky92@gmail.com>
2015-02-11 19:06:30 -0500

    structured a grunt file and added grunt-contrib-connect to use the construct a server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,35 @@
 module.exports = function(grunt) {
 
+
+// Initial configuration
+// ========================================================
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: { // task name
+        options: {
+          port: "8000",
+          protocol: "http",
+          hostname: "localhost",
+          base: './app',
+          keepalive: true,
+          livereload: true,
+          open: true
+        }
+      }
+    }
   });
+// ========================================================
+
+// Loading npm tasks
+// ========================================================
+  grunt.loadNpmTasks('grunt-contrib-connect');
+// ========================================================
+
+// Register npm tasks
+// ========================================================
+  grunt.registerTask('default', ['connect']);
+// ======================================================== 
+
+
 };
